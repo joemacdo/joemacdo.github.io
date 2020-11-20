121bb13aafa34f60696cd1240c4e6f80477b1c7c
joemacdo <70913122+joemacdo@users.noreply.github.com>
2020-11-20 16:07:38 -0600

    Add files via upload
    changed from resume to fantasy football site

---
+++ animate.js
@@ -0,0 +1,22 @@
+console.log('js');
+
+function DoAnimationR() {
+  var targetElement = document.getElementById("munsch");
+  targetElement.className = "animate-r";
+}
+
+function DoAnimationL() {
+var targetElement = document.getElementById("chops");
+targetElement.className = "animate-l";
+}
+
+// function animateLoser() {
+//   targetElement.className = "loser";
+// }
+//
+// function animateJason() {
+//   var targetElement = document.getElementById("jason");
+//   DoAnimationR();
+//   animateLoser();
+// 
+// }
